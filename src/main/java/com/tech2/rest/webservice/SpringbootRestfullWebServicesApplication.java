43299f0227c9c56de40e71656c4ab8fc6709baa4
Ram Pravesh <rampravesh07@gmail.com>
2018-11-01 10:41:07 +0530

    First commit

---
+++ src/main/java/com/tech2/rest/webservice/SpringbootRestfullWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.tech2.rest.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullWebServicesApplication.class, args);
+	}
+}
